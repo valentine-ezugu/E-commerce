20316625a69654084fde06b21489ced5046ecd4e
valentine <valentineezugu@yahoo.com>
2018-01-22 02:39:40 +0300

    commit testing

--- src/main/java/com/bookstore/BookstoreApplications.java
+++ src/main/java/com/bookstore/BookstoreApplications.java
@@ -8,10 +8,8 @@ import com.bookstore.utility.SecurityUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.HashSet;
@@ -20,7 +18,7 @@ import java.util.Set;
 @Configuration
 @SpringBootApplication
 @EnableTransactionManagement
-public class BookstoreApplications implements CommandLineRunner  {
+public class BookstoreApplications implements CommandLineRunner {
 
     @Autowired
     private UserService userService;
@@ -29,19 +27,19 @@ public class BookstoreApplications implements CommandLineRunner  {
         SpringApplication.run(BookstoreApplications.class, args);
     }
 
-        @Override
-        public void run (String...strings) throws Exception {
-            User user1 = new User();
-            user1.setFirstName("Valentine");
-            user1.setLastName("Ezugu");
-            user1.setUsername("V");
-            user1.setPassword(SecurityUtility.passwordEncoder().encode("A"));
-            user1.setEmail("valentineezugu@yahoo.com");
-            Set<UserRole> userRoles = new HashSet<>();
-            Role role1 = new Role();
-            role1.setRoleId(1);
-            role1.setName("USER");
-            userRoles.add(new UserRole(user1, role1));
-            userService.createUser(user1, userRoles);
-        }
+    @Override
+    public void run(String... strings) throws Exception {
+        User user1 = new User();
+        user1.setFirstName("Valentine");
+        user1.setLastName("Ezugu");
+        user1.setUsername("V");
+        user1.setPassword(SecurityUtility.passwordEncoder().encode("A"));
+        user1.setEmail("valentineezugu@yahoo.com");
+        Set<UserRole> userRoles = new HashSet<>();
+        Role role1 = new Role();
+        role1.setRoleId(1);
+        role1.setName("USER");
+        userRoles.add(new UserRole(user1, role1));
+        userService.createUser(user1, userRoles);
+    }
 }
